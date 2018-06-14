a9cfda203d84c524587b55a6071dd8c8fdf2ee88
Steve Strong <sstrong@apprenticesystems.com>
2018-06-13 23:46:47 -0400

    one more tweek

--- ngFoundrySignal/Program.cs
+++ ngFoundrySignal/Program.cs
@@ -19,6 +19,7 @@ namespace ngFoundrySignal
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+             .UseContentRoot(Directory.GetCurrentDirectory())
+             .UseStartup<Startup>();
     }
 }
